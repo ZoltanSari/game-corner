f66904a38aa740fbfe334e09de4d7815b89f8eda
ZoltanSari <Kingshapy@gmail.com>
2019-02-15 11:44:12 +0100

    create enum Genre with genres

---
+++ backend/src/main/java/com/sari/gamecorner/model/Genre.java
@@ -0,0 +1,19 @@
+package com.sari.gamecorner.model;
+
+public enum Genre {
+    SHOOTER,
+    SIMULATOR,
+    ADVENTURE,
+    ROLE_PLAYING_RPG,
+    SPORT,
+    STRATEGY,
+    TURN_BASED_STRATEGY_TBS,
+    TACTICAL,
+    FIGHTING,
+    HACK_AND_SLASH_PER_BEAT_EM_UP,
+    REAL_TIME_STRATEGY_RTS,
+    POINT_AND_CLICK,
+    PUZZLE,
+    RACING,
+    PINBALL
+}
