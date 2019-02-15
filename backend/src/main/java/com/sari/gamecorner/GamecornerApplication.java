985fe002758933631a69796db401447c80f2be1b
ZoltanSari <Kingshapy@gmail.com>
2019-02-15 10:36:18 +0100

    create springboot project

---
+++ backend/src/main/java/com/sari/gamecorner/GamecornerApplication.java
@@ -0,0 +1,14 @@
+package com.sari.gamecorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecornerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamecornerApplication.class, args);
+    }
+
+}
+
---
+++ backend/src/test/java/com/sari/gamecorner/GamecornerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sari.gamecorner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamecornerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
