1e8db7a0894ee2c5fecd5b7b0a99c063d0be6d2d
ZoltanSari <Kingshapy@gmail.com>
2019-02-15 15:03:28 +0100

    create model for the game

---
+++ frontend/src/app/models/Game.ts
@@ -0,0 +1,16 @@
+export class Game {
+
+  id: number;
+  name: string;
+  storyline: string;
+  summary: string;
+  trailer: string;
+  genres: string[];
+  gameModes: string[];
+  platforms: string[];
+  coverUrl: string;
+  firstReleaseDate: string;
+  company: string;
+  rating: string;
+
+}
