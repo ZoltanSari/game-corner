c5713f45f51533538e149d0fab655c443947bb41
ZoltanSari <Kingshapy@gmail.com>
2019-02-15 11:51:14 +0100

    create platform enum with all platform

---
+++ backend/src/main/java/com/sari/gamecorner/model/Platform.java
@@ -0,0 +1,27 @@
+package com.sari.gamecorner.model;
+
+public enum Platform {
+    XBOX,
+    XBOX_360,
+    XBOX_ONE,
+    PLAYSTATION,
+    PLAYSTATION_1,
+    PLAYSTATION_2,
+    PLAYSTATION_3,
+    PLAYSTATION_4,
+    IOS,
+    LINUX,
+    PC_MICROSOFT_WINDOWS,
+    MAC,
+    PC_DOS,
+    NINTENDO_DS,
+    NINTENDO_DSI,
+    AMIGA,
+    STEAMOS,
+    GAME_BOY_ADVANCE,
+    ANDROID,
+    PLAYSTATION_NETWORK,
+    ATARI_ST_PER_STE,
+    APPLE_II,
+    APPLE_IIGS
+}
