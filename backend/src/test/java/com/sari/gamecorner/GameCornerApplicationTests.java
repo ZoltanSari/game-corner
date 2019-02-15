ad43996e5e02b617a2318e5cd818d188ca438161
ZoltanSari <Kingshapy@gmail.com>
2019-02-15 11:33:23 +0100

    rename the application name

--- backend/src/test/java/com/sari/gamecorner/GameCornerApplicationTests.java
+++ backend/src/test/java/com/sari/gamecorner/GameCornerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GamecornerApplicationTests {
+public class GameCornerApplicationTests {
 
     @Test
     public void contextLoads() {
