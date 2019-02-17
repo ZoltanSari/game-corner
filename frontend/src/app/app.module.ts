a241bf26336f3f2c6ca8d612c152db1e79155225
ZoltanSari <Kingshapy@gmail.com>
2019-02-17 16:34:04 +0100

    add declarations and import to the app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { GameDetailsComponent } from './components/game/game-details/game-details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {GameService} from "./services/game.service";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GameDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
