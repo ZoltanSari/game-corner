779fdda87d9ab85cecedc91a8c0cea7ed3ba2330
ZoltanSari <Kingshapy@gmail.com>
2019-02-17 16:31:31 +0100

    rename game.ts as game.model

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError, tap } from "rxjs/operators";
 import { of } from "rxjs";
 import { Game } from "../models/game.model";
 
