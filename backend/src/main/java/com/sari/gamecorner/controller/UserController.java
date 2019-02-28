681f14c0fc20424bffeee326fbe2bea94274721d
ZoltanSari <Kingshapy@gmail.com>
2019-02-28 11:45:00 +0100

    send error message and http status to the frontend

--- backend/src/main/java/com/sari/gamecorner/controller/UserController.java
+++ backend/src/main/java/com/sari/gamecorner/controller/UserController.java
@@ -22,6 +22,7 @@ public class UserController {
         if (userService.handleRegistration(userDTO)) {
             return new ResponseEntity(HttpStatus.OK);
         }
+
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(ErrorDTO.builder()
                         .error("invalid credentials")
@@ -31,7 +32,6 @@ public class UserController {
 
     @PostMapping("/login")
     public User login(@RequestBody UserDTO userDTO) {
-
         return userService.login(userDTO);
     }
 
@@ -41,18 +41,35 @@ public class UserController {
     }
 
     @PutMapping("/{username}/liked-games/{gameId}")
-    public void toggleGameInLikedGames(@PathVariable String username, @PathVariable long gameId) {
-        userService.toggleGameInLikedGames(username, gameId);
+    public ResponseEntity toggleGameInLikedGames(@PathVariable String username, @PathVariable long gameId) {
+        if (userService.toggleGameInLikedGames(username, gameId)) return new ResponseEntity(HttpStatus.OK);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ErrorDTO.builder()
+                        .error("invalid parameters")
+                        .message("Username or game id is invalid in the URL")
+                        .build());
     }
 
     @PutMapping("/{username}/walkthrough-games/{gameId}")
-    public void toggleGameInWalkthroughGames(@PathVariable String username, @PathVariable long gameId) {
-        userService.toggleGameInWalkthroughGames(username, gameId);
+    public ResponseEntity toggleGameInWalkthroughGames(@PathVariable String username, @PathVariable long gameId) {
+        if (userService.toggleGameInWalkthroughGames(username, gameId)) return new ResponseEntity(HttpStatus.OK);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ErrorDTO.builder()
+                        .error("invalid parameters")
+                        .message("Username or game id is invalid in the URL")
+                        .build());
     }
 
     @PutMapping("/{username}/game-list/{gameId}")
-    public void toggleGameInGameList(@PathVariable String username, @PathVariable long gameId) {
-        userService.toggleGameInGameList(username, gameId);
-    }
+    public ResponseEntity toggleGameInGameList(@PathVariable String username, @PathVariable long gameId) {
+        if (userService.toggleGameInGameList(username, gameId)) return new ResponseEntity(HttpStatus.OK);
 
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ErrorDTO.builder()
+                        .error("invalid parameters")
+                        .message("Username or game id is invalid in the URL")
+                        .build());
+    }
 }
