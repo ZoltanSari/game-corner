1d1a2837a28ed3254ecbc9e67b4ac2b97121b8c8
ZoltanSari <Kingshapy@gmail.com>
2019-03-07 10:54:14 +0100

    add registration path to the route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {GameDetailsComponent} from "./components/game/game-details/game-details.
 import { GameListComponent } from "./components/game/game-list/game-list.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {HomePageComponent} from "./components/home-page/home-page.component";
+import {RegistrationComponent} from "./components/registration/registration.component";
 
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'games', component: GameListComponent},
   {path:'games/search', component: GameListComponent},
+  { path: 'registration', component: RegistrationComponent},
   { path: 'not-found', component: PageNotFoundComponent, data: {message: 'Page not found!'} },
   { path: ':id', component: GameDetailsComponent },
   { path: '**', redirectTo: '/not-found' }
